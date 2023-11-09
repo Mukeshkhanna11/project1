4da6c783443892a9d3af1c78c717de2b4e1dca0a
Mukeshkhanna11 <mukeshmass.9865@gmail.com>
2023-11-09 16:08:00 +0530

    modified app.js and user.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import "./App.css";
 import Message from "./Message";
+import User from "./User";
 
 const App = () => {
+  const age = 15;
+  const isGreen = true;
+  const colorMessage = () => {
+    if (isGreen === false) {
+      return "red";
+    } else {
+      return "green";
+    }
+  };
+  const users = [
+    { name: "ragesh", age: 28 },
+    { name: "ramesh", age: 28 },
+  ];
+
   return (
     <div className="App">
       <Message />
+      <div>{age >= 18 ? <h1>over age</h1> : <h1>under age</h1>}</div>
+      <h1 style={{ color: isGreen ? "green" : "red" }}>
+        color is {colorMessage()}
+      </h1>
+      {users.map((user) => {
+        return <User name={user.name} age={user.age} />;
+      })}
     </div>
   );
 };
--- src/Message.js
+++ src/Message.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 const Message = () => {
   const [message, setMessage] = useState("wellcome visitor");
+
   return (
     <div>
       <h1>{message}</h1>
